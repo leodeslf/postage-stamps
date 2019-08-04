5c0bbf94e016b16c3e0a027ab9f39254fbcea748
Leo <leodesouzauy@hotmail.com>
2019-08-04 02:50:48 -0300

    first one

---
+++ src/APS.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function APS() {
+  return (
+    <div className="APS">
+
+    </div>
+  );
+}
+
+export default APS;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import APS from './APS';
+
+render(<APS />, document.getElementById('root'));
